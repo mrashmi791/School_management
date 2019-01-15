dd17706b0e73039f87127ac71fedd5aa8e22b898
Rashmi rani <mrashmi791@gmail.com>
2019-01-16 00:06:29 +0530

    initial commit

---
+++ js/gallary.js
@@ -0,0 +1,21 @@
+
+function enlargeImage(img_id) {
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById(img_id);
+    var modalImg = document.getElementById("img01");
+
+        modal.style.display = "block";
+        modalImg.src = img.src;
+
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+}
